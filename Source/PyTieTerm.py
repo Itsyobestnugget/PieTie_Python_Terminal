2a6beba523d8e939b1efd41d9279570e1fbd0737
Kai Kawaii <103155299+Itsyobestnugget@users.noreply.github.com>
2024-10-05 03:12:04 +0000

    Added CallBackLog for log framework made it more user friendly updated commands

--- Source/PyTieTerm.py
+++ Source/PyTieTerm.py
@@ -1,13 +1,27 @@
 import os
 
+termon = True
+
+os.system('clear')
+print("   |\---/| \n   | ,_, |\n    \_w_/-..----.\n ___/ `   ' ,""+ \  BC\n(__...'   __\    |`.___.';\n  (_,...'(_,.`__)/'.....+\n\n Welcome to PyTie_TERMON Nya~ \n PyTie_TERMON, Better known as TERMON is a use friendly command prompt powered terminal, made in python\n by a guy with no coding expirience in python...\n Im BoxCat the terminals mascot. i like to run things around here \n Type help for help NOTE: TERMON is CASE SENSITIVE meaning commands need to be the exact capitalization as displayed!\n\n")
+
+def CBP(reason):
+        BSODerror = reason
+        print("\n \nCALLBACKPING")
+        print("  ,-.       _,---._ __  / \ \n /  )    .-'       `./ /   \ \n(  (   ,'            `/    /|                         \n  `.              ,  \ \ /  | \n   /`.          ,'-`----Y   | \n  (            ;        |   ' \n  |  ,-.    ,-'         |  / \n  |  | (   |    oops... | / \n  )  |  \  `.___________|/ \n  `--'   `--') \n \n Apparently, the terminal ran into a problem... thats not good\n this would be where we would collect data. but we dont at the moment, see reason for crash below \n \n")
+        print("🠇🠇🠇 Reason 🠇🠇🠇\n")
+        print(BSODerror)
+        print("\n🠅🠅🠅 Reason 🠅🠅🠅")
+        exit()
+
 while True:
     dir_path = os.path.dirname(os.path.realpath(__file__))
     cmd = input("C:/user/PyTie.exe : ")
     if cmd == "help":
-        print("PyTie is CASE SENSITIVE!!!!!!!! \n \n help - Displays all commands \n")
+        print("TERMON_PyTie is CASE SENSITIVE!!!!!!!! \n \n help - Displays all commands \n")
     
     elif cmd == "dir":
-        print("Displaying directorys...\n \n")
+        print("Displaying directorys and files...\n \n")
         directory = dir_path
         directorys = os.path.dirname
         for root, dirs, files in os.walk(directory):
@@ -15,10 +29,12 @@ while True:
                 print (os.path.join(root, subFile, directorys))
 
     elif cmd == "cd ..":
-        print("error going back")
+        CBP("ERR_KEEP_UP")
 
     elif cmd == "mkfile":
-        print("This feature is currently in development, please wait for it to be released")
+        print("This feature is currently in development, please wait for it to be released \n")
+        CBP("ERR_KEEP_UP")
+#        print("This feature is currently in development, please wait for it to be released")
 #        print("Welcome to MKFile (make file) function which makes a text file. \n \n lets make a new file. first select a name")
 #        Filename = "/workspaces/PieTie_Python_Terminal/Source/text.txt".join((input("Enter a name: ")))
 #        Content = input("now lets insert some data. type it here: ")
@@ -44,7 +60,17 @@ while True:
         fileopen = open(input("File.. with dir eg /workspaces/PieTie_Python_Terminal/Source/File.txt   : "), "r")
         print(fileopen.read())
 
-    elif cmd == "placeholdercommand":
+    elif cmd == "Cls":
+        print("Clearing screen...")
+        os.system('clear')
+
+    elif cmd == "callbackping":
+        CBP(input("Reason: "))
+
+    elif cmd == "errdisplay":
+        print("ERR_KEEP_UP\n   This error means that this feature is either depricated, or not finished.\n to stop any errors out of development we use a CBP to replace the function to make sure the TERMINAL isnt effected.")
+
+    elif cmd == "":
         print("Placeholder")
 
     elif cmd == "placeholdercommand":
@@ -79,14 +105,5 @@ while True:
 
     elif cmd == "placeholdercommand":
         print("Placeholder")
-
-    elif cmd == "placeholdercommand":
-        print("Placeholder")
-
-    elif cmd == "placeholdercommand":
-        print("Placeholder")
-
-    elif cmd == "placeholdercommand":
-        print("Placeholder")
-
-
+    elif cmd == "":
+        print("")
